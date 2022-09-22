c4bc8dd8b5161dc128d2c679c62ea453d24fa06c
Zac Poorman <33203487+OOCAZ@users.noreply.github.com>
2022-09-22 15:28:34 -0600

    added number representation to number list, just now to add alerts

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -3,15 +3,31 @@ const express = require('express');
 const router = express.Router();
 const numbersTemplateCopy = require('../models/NumbersModels');
 
-router.post('/number', (request, response) => {
+router.post('/add', (request, response) => {
     const numbersInit = new numbersTemplateCopy({
         number:request.body.number,
     })
     numbersInit.save()
     .then(data =>{
-        respoonse.json(data)
+        response.json(data)
     }).catch(err => 
         response.json(err))
+})
+
+router.post('/remove', (request, response) => {
+     const numbersInit = new numbersTemplateCopy({
+        number:request.body.number,
+    })
+    numbersInit.collection.deleteMany({"number": request.body.number})
+    .then(data =>{
+        response.json(data)
+    }).catch(err => 
+        response.json(err))
+})
+
+router.get('/numbers', async (request, response) => {
+    const numbers = await numbersTemplateCopy.find()
+    response.send(numbers)
 })
 
 module.exports = router
--- src/App.js
+++ src/App.js
@@ -1,18 +1,78 @@
 import openhandweb from './openhandweb.png'
 import './App.css';
-import React from 'react';
-import { Button, TextField } from '@mui/material';
+import React, {useEffect} from 'react';
+import { Button, TextField, ThemeProvider, createTheme, ListItem, List } from '@mui/material';
 import Image from 'mui-image';
+import axios from 'axios';
 
 function App() {
 
-const [numbers, setNumbers] = React.useState([])
-const [currentNumber, setCurrentNumber] = React.useState(String)
+  const [numbers, setNumbers] = React.useState([])
+  const [currentNumber, setCurrentNumber] = React.useState(String)
+  const [toggle, setToggle] = React.useState(true);
 
-const handleChange = (event) => {
-    setCurrentNumber(event.target.value);
-    console.log(event.target.value)
+  const handleToggle = () => {
+    setToggle(!toggle);
   };
+
+  async function fetchData() {
+      try{
+      const res = await axios.get('http://localhost:4000/app/numbers');
+                var temp1 = ""
+                Object.entries(res.data).forEach((entry) => {
+                  const [key, value] = entry;
+                  temp1 = temp1 + " " + value.number.toString()
+                });
+                setNumbers(temp1);
+            } catch (err) {
+                console.log(err);
+            }
+  }
+    useEffect(() => {
+        fetchData();
+    }, [toggle])
+
+  
+  const handleChange = (event) => {
+      setCurrentNumber(event.target.value);
+      console.log(event.target.value)
+    };
+  
+    const theme = createTheme({
+      palette: {
+        mode: 'dark',
+      },
+    });
+
+  function getListNumbers(){
+        fetchData();
+        console.log(numbers)
+  }
+
+  function onAddNumber(){
+    const addNumber = {
+      number: currentNumber
+    }
+
+    axios.post('http://localhost:4000/app/add', addNumber)
+    .then(response => console.log(response.status))
+    setCurrentNumber('');
+    getListNumbers();
+    return
+  }
+
+  function onRemoveNumber(){
+    const removeNumber ={
+      number: currentNumber
+    }
+
+    axios.post('http://localhost:4000/app/remove', removeNumber)
+    .then(response => console.log(response.status))
+    
+    setCurrentNumber('');
+    getListNumbers();
+    return
+  }
 
   return (
     <div className="App">
@@ -24,9 +84,11 @@ const handleChange = (event) => {
         <h2>
           Numbers Ready: {numbers}
         </h2>
-        <TextField id="outlined-basic" label="Number" variant="outlined" onChange={handleChange} value={currentNumber} />
-        <Button variant="contained" sx={{mt: 2}}>Add Number</Button>
-        <Button variant="contained" sx={{mt: 2}}>Remove Number</Button>
+        <ThemeProvider theme={theme}>
+          <TextField id="outlined-basic" label="Number" variant="outlined" onChange={handleChange} value={currentNumber} />
+        </ThemeProvider>
+        <Button variant="contained" sx={{mt: 2}} onClick={onAddNumber}>Add Number</Button>
+        <Button variant="contained" sx={{mt: 2}} onClick={onRemoveNumber}>Remove Number</Button>
       </header>
       
     </div>
